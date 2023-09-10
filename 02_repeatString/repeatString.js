b7bceb7573ea77b687a0599ad25398df24743dba
hansenlau <sen_lau@hotmail.com>
2023-09-10 15:16:00 +0700

    Add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString(string,num) {
+    let result ="";
+    if (num<0) result = "ERROR";
+    else if (num>=0) for (let i=0; i<num; i++) {
+        result += string;};
+    return result;
+    };
+  
 
-};
+   
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -1,10 +1,12 @@
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
-  }
-  return string;
-};
+function repeatString(string,num) {
+  let result ="";
+  if (num<0) result = "ERROR";
+  else if (num>=0) for (let i=0; i<num; i++) {
+      result += string;};
+  return result;
+  };
+
+  repeatString("hey",3);
+
 
 module.exports = repeatString;
