570db3a55d87163c874a1f540dd9f30c6a146f17
hansenlau <sen_lau@hotmail.com>
2023-09-14 14:41:42 +0700

    Add alternative solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,3 +1,5 @@
+// solution 1 by Hansen
+/*
 function removeFromArray() {
     let array = arguments[0];
     for (i=1; i < arguments.length; i++){
@@ -7,6 +9,26 @@ function removeFromArray() {
         }
     }
     return array;
+};
+*/
+
+//solution 2
+/* function removeFromArray(array, ...args){
+    const newArray = [];
+    array.forEach((item)=>{
+        if (!args.includes(item)){
+            newArray.push(item)
+        }
+
+    }
+
+    )
+    return newArray;
+}
+*/
+
+function removeFromArray(array,...args){
+    return array.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
