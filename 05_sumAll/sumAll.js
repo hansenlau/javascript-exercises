9a05e890766105e0b6544cd53c5e7c4ee507095b
hansenlau <sen_lau@hotmail.com>
2023-09-15 14:24:02 +0700

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+let sum = 0;
+
+const sumAll = function(firstNum, lastNum) {
+  if (typeof firstNum !== "number" || typeof lastNum !== "number") return "ERROR";
+  else if (firstNum < 0 || lastNum <0) return "ERROR";
+  else if (firstNum > lastNum) {
+     n = firstNum - lastNum +1;
+  } else n = lastNum - firstNum + 1;
+
+  return (n/2)*(firstNum+lastNum);
 
 };
 
