19f0bb92a94b6ab9c1fb72b1129596269097a3d4
hansenlau <sen_lau@hotmail.com>
2023-09-16 10:12:35 +0700

    Add solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const year400 = year%400 == 0;
+    const year4 = year%4 == 0;
+    const year100 = year % 100 == 0;
 
-};
+if (year400) return true;
+else if (year100) return false;
+else if (year4) return true;
+return false;
+            
+
+    };
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
