a1b2febf49f83cd8fca5e053d087ad897f82f77e
hansenlau <sen_lau@hotmail.com>
2023-09-21 18:24:18 +0700

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(input) {
+    let fibbo = [1, 1];
+    let index= Number(input)-1;
+    if (input<=0) return "OOPS";
+    for (let i = 2; i <= index; i++){
+        fibbo.push(fibbo[i-1]+fibbo[i-2]);
+        console.log(fibbo);
+    }
+    return fibbo[index];
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
