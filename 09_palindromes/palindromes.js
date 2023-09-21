8e8bf0b453d2179548b3ad157d3fbe88f6d7704b
hansenlau <sen_lau@hotmail.com>
2023-09-21 10:59:22 +0700

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let punc = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    let whiteSpace = (/\s/g, '')
+    let initWord = input.replace(/\s/g, '').replace(punc,'').toLowerCase();
+    let finWord = input.replace(punc,"").replace(/\s/g, '').toLowerCase().split('').reverse().join('');
+    console.log(typeof(input));
+    console.log(initWord);
+    console.log(finWord);
+    if (initWord == finWord) return true;
+    else return false;
 
 };
 
