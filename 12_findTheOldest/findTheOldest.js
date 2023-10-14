6ea56f48db779592752ff247a9b3cf52b31314da
hansenlau <sen_lau@hotmail.com>
2023-10-14 20:40:07 +0700

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,22 @@
 const findTheOldest = function(people) {
 
-const oldest = people.map(a=>(a.yearOfDeath-a.yearOfBirth)).reduce((a,b)=>{
- if (a<b) return b;
- else return a;
-},0);
-const ageArray = people.map(a=>(a.yearOfDeath-a.yearOfBirth));
-console.log(oldest);
-console.log(people.map(a=>(a.yearOfDeath-a.yearOfBirth)));
+const d = new Date();
+let year = d.getFullYear();
+    
+const ageArr = people.map(a=>{
+    if(isNaN(a.yearOfDeath)) return (year - a.yearOfBirth);
+    else return (a.yearOfDeath-a.yearOfBirth);
+    });
+    
+const oldest =  ageArr.reduce((a,b)=>{
+    if (a<b) return b;
+    else return a;
+    },0);
 
-const index = ageArray.indexOf(oldest);
-console.log(people[index]);
+const index = ageArr.indexOf(oldest);
+
 return people[index];
+
 
 };
 
